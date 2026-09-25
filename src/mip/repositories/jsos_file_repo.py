fe696ad447bf911c3b8ca7e7fb9b90417d5e49f8
nikhildelhi8 <nikhildelhi8@gmail.com>
2026-09-25 08:08:52 +0530

    refact: changed configuration in json_file_repo

--- src/mip/repositories/jsos_file_repo.py
+++ src/mip/repositories/jsos_file_repo.py
@@ -1,7 +1,13 @@
 from typing import TypeVar , Generic , Callable 
 from pathlib import Path
-from mip.exceptions import NotFoundError , DuplicateError , ValidationError , MIPError
+from mip.exceptions import NotFoundError , DuplicateError , ValidationError , MIPError , RepositoryError
 import json
+import os , tempfile
+
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 
@@ -18,15 +24,15 @@ class JsonFileRepo(Generic[T]):
         self.from_dict = from_dict
         self._data : dict[str , T] = {}
 
-        if not self.path.exists():
-            self.path.parent.mkdir(parents=True , exist_ok=True)
+        self.path.parent.mkdir(parents=True , exist_ok=True)
 
-        self._load()
+        self.__load()
 
 
-    def _load(self) -> None :
+    def __load(self) -> None
 
         loaded_json = {}
+        failed_parsed_record = []
 
         if not self.path.exists():
             self._data = {}
@@ -38,20 +44,91 @@ class JsonFileRepo(Generic[T]):
                 loaded_json = json.load(f)
             
             except  json.JSONDecodeError as e :
-                raise NotFoundError(
+                raise RepositoryError(
                     f"path : '{self.path}' does not exist check it getting error : {e} "
                 )
 
-        try :
-            for item in loaded_json :
-                    entity_id  = self.get_id(item)
-                    raw        = self.from_dict(item)
-                    self._data[entity_id] = raw
+        
+        # for entity_id , item in loaded_json.items() :
+
+        #     try : 
+        #         validatedObject = self.from_dict(item)
+        #         self._data[entity_id] = validatedObject
+
+        #     except Exception as e  :
+        #         raise RepositoryError(
+        #             f"got error while converting {entity_id} dict  to dataclass object getting {e}"
+        #         )
+
+        failed_parsed_record = []
+        
+        for entity_id , item in loaded_json.items() :
+
+            try:     
+                validatedObject = self.from_dict(item)
+
+            except Exception as e :
+                logger.warning(f"parsing failed for {entity_id}")
+                failed_parsed_record.append({entity_id: item})
+                continue
+
+            self._data[entity_id] = validatedObject
+
+       
+
+
+    def __flush(self , path: Path) -> None :
+
+        failed_json_parsed_record = []
+        json_safe_dict = {}
+
+        for entity_id , entity in self._data.items():
+
+            try:
+                json_safe_dict = self.to_dict(entity)
+
+            except Exception as e :
+
+                logger.warning(f"Json parsing failed for {entity_id}")
+                failed_json_parsed_record.append({entity_id : entity})
+                continue
+
+        if len(failed_json_parsed_record) > 0 :
+            logger.warning()
+
+       # atomic writing to temp file and then replacing it with original file 
+
+        fd , tmp_path = tempfile.mkstemp(dir=path.parent)
+
+        try:
+            with os.fdopen(fd , 'w') as f:
+                json.dump(json_safe_dict , f)
+            os.replace(tmp_path , path)
 
         except Exception as e :
-            raise MIPError(
-                f"got error while converting loaded_json to dataclass object getting {e}"
+            raise RepositoryError(
+                f" writing to file is having some issues getting {e}"
             )
+        finally :
+            if os.path.exists(tmp_path):
+                os.remove(tmp_path)
+        
+
+        
+
+        
+
+
+
+
+
+        
+              
+            
+                
+
+        
+        
         
 
 
