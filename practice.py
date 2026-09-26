6e9e823810d0b5b499ed8cd7ef13d1b4e0683d1c
nikhildelhi8 <nikhildelhi8@gmail.com>
2026-09-26 12:57:18 +0530

    modified .gitignore

--- practice.py
+++
@@ -1,9 +0,0 @@
-import csv 
-from pathlib import Path 
-
-
-def read_rows(path: Path) : 
-    with open(path , newline="" , encoding = "utf-8") as f :
-        reader = csv.DictReader(f)
-        for row in reader:
-            print(row)
